cf18e12761fad50ba05b78f9c621e348147849f1
Dennis Williams <dlwjr0103@gmail.com>
2021-12-08 18:52:50 -0500

    AJAX Basics lesson 6.1

--- JS/index.js
+++ JS/index.js
@@ -6,7 +6,7 @@ const copyright = document.createElement('p')
 copyright.innerHTML = "&copy; Dennis Williams " + thisYear ; 
 footer.appendChild(copyright);
 
-let skills = ['JavaScript','HTML','Project Management'];
+let skills = ['JavaScript','HTML','Project Management','CSS'];
 let skillsSection = document.querySelector('#skills')
 let skillsList = skillsSection.querySelector('ul')
 
@@ -45,3 +45,29 @@ messageForm.addEventListener("submit", (event) => {
 }
 
 );
+ 
+var githubRequest = new XMLHttpRequest();
+     githubRequest.open("GET", "https://api.github.com/users/Dlwjr0103/repos"); 
+     githubRequest.send();
+    
+ githubRequest.addEventListener("load", function(){
+     if (githubRequest.readyState === 4){
+      if (githubRequest.status ===200){
+           console.log("ok")
+     let repositories = JSON.parse(this.response);
+      console.log(repositories);
+
+
+ const projectSection = document.querySelector('#projects')
+ const projectList = projectSection.querySelector('ul')
+
+ for (let i = 0; i < repositories.length; i++){
+
+     const project = document.createElement('li')
+     project.innerText= repositories[i].name;
+     projectList.appendChild(project)
+     }
+    }
+   }
+ })
+
